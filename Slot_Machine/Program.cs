99a2cbd92d1470d461c2a382960637f806cb6166
thirdiblind <65646654+thirdiblind@users.noreply.github.com>
2023-12-12 20:42:22 +0000

    Created GameLogic class, created selectBet method

--- Slot_Machine/Program.cs
+++ Slot_Machine/Program.cs
@@ -9,6 +9,7 @@ namespace Slot_Machine
         static void Main(string[] args)
         {
 			GameUI ui = new GameUI();
+			GameLogic logic = new GameLogic();
             const int SLOT_MACHINE_LENGTH = 3;
             const int CENTER_ROW = (SLOT_MACHINE_LENGTH - 1) / 2;
             const int RANDOM_MAX = 9;
@@ -33,27 +34,8 @@ namespace Slot_Machine
                 
                 while (true)
                 {
-                    Console.Write("Enter your bet: 1,2,3 or 4: ");
-                    string input = Console.ReadLine();
-                    if (int.TryParse(input, out int result) && result > 0 && result < 5)
-                    {
-                        if (bet <= balance)
-                        {
-                            bet = result;
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Insufficient balance for this bet.");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid input. Please enter a number between 1 and 4");
-                    }
+					bet = GameLogic.selectBet("Enter your bet: 1,2,3 or 4: ", balance);
 
-                }
-                
                 Console.WriteLine($"You have bet {bet} credits.");
 
                 balance = balance - bet;
@@ -209,4 +191,24 @@ namespace Slot_Machine
 		Console.WriteLine("-----------------------------------------------------------------------------");
 		}
 	}
+	public class GameLogic
+	{
+		public static int selectBet(string prompt, int balance)
+		{
+			while (true) // Loop infinitely until a break or return. Used to continuously prompt for input until a valid integar is entered.
+       		{
+			Console.WriteLine(prompt);
+			string input = Console.ReadLine();
+			if(int.TryParse(input, out int bet) && bet > 0 && bet < 5)
+			{
+			   if (bet <= balance)
+			   {
+				   return bet;
+			   }
+			}
+			Console.WriteLine("Invalid Input. Please enter a valid bet, from 1-4.");
+			}
+		}
+	}
+}
 }
